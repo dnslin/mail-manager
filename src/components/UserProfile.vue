d791cc543c994e6ef2630efd122fc8c82baf2a59
dnslin <i@dnsl.in>
2024-03-01 17:17:53 +0800

    feat: 个人信息修改

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -3,10 +3,10 @@
         <div class="task-manager">task manager</div>
         <div class="side-wrapper">
             <div class="user-profile">
-                <img src="https://assets.codepen.io/3364143/Screen+Shot+2020-08-01+at+12.24.16.png" alt=""
-                    class="user-photo">
-                <div class="user-name">Natalie Smith</div>
-                <div class="user-mail">natalie.smith@gmail.com</div>
+                <a-avatar @click="handleClick" :size="81">
+                    <img alt="avatar" style="object-fit: cover;" :src=userInfo.url /></a-avatar>
+                <div class="user-name">{{ userInfo.name }}</div>
+                <div class="user-mail">{{ userInfo.email }}</div>
             </div>
             <UserNotification />
             <div class="progress-status">12/34</div>
@@ -17,12 +17,52 @@
         </div>
         <Projects />
         <Team />
+        <a-modal v-model:visible="visible" title="个人信息" @cancel="handleCancel" @before-ok="handleBeforeOk">
+            <a-form :model="userInfo">
+                <a-avatar :size="108" class="avatar-form-item">
+                    <img alt="avatar" style="object-fit: cover;" :src=userInfo.url /></a-avatar>
+                <a-form-item field="name" label="昵称:">
+                    <a-input v-model="userInfo.name" />
+                </a-form-item>
+                <a-form-item field="email" label="邮箱地址:">
+                    <a-input v-model="userInfo.email" />
+                </a-form-item>
+            </a-form>
+        </a-modal>
     </div>
 </template>
 
 <script setup>
+import { reactive, ref } from "vue";
 import UserNotification from './UserNotification.vue';
 import TaskStatus from './TaskStatus.vue';
 import Projects from './Projects.vue';
 import Team from './Team.vue';
+
+const userInfo = reactive({
+    name: '林祈',
+    email: 'i@dnsl.in',
+    url: 'https://assets.codepen.io/3364143/Screen+Shot+2020-08-01+at+12.24.16.png',
+});
+const visible = ref(false);
+
+const handleClick = () => {
+    visible.value = true;
+};
+const handleBeforeOk = (done) => {
+    window.setTimeout(() => {
+        done();
+        // prevent close
+        // done(false)
+    }, 1000);
+};
+const handleCancel = () => {
+    visible.value = false;
+};
 </script>
+
+<style scoped>
+.avatar-form-item {
+    margin: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ArcoVue);
+app.mount('#app');
+
