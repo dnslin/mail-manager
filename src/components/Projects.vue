fb87a2f4e1355199c5b5bf84edabf9f13c3d7ad0
dnslin <i@dnsl.in>
2024-03-03 22:49:29 +0800

    fix: 修改tag

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,7 +2,7 @@
     <div class="side-wrapper">
         <div class="project-title">标签列表</div>
         <div class="project-name">
-            <div class="project-department">垃圾游戏</div>
+            <div class="project-department">垃圾广告</div>
             <div class="project-department">论坛网站</div>
             <div class="project-department">科技周刊</div>
             <div class="project-department">服务订阅</div>
