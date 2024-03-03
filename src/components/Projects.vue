fb1a5e5648b12a565564b05eb88e6b036f9f5c50
dnslin <i@dnsl.in>
2024-03-03 23:03:30 +0800

    feat: 优化代码

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,10 +2,20 @@
     <div class="side-wrapper">
         <div class="project-title">标签列表</div>
         <div class="project-name">
-            <div class="project-department">垃圾广告</div>
-            <div class="project-department">论坛网站</div>
-            <div class="project-department">科技周刊</div>
-            <div class="project-department">服务订阅</div>
+            <div class="project-department" v-for="(project, index) in projectList" :key="index">{{ project.department
+                }}</div>
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const projectList = ref([
+    { key: '垃圾广告', department: '垃圾广告' },
+    { key: '论坛网站', department: '论坛网站' },
+    { key: '科技周刊', department: '科技周刊' },
+    { key: '服务订阅', department: '服务订阅' }
+])
+
+</script>
