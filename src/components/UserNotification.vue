a7bd3b8de0b3b4aea922dd56710dd33bfe775d3b
dnslin <i@dnsl.in>
2024-03-05 09:08:57 +0800

    fix: 修复系统通知报错

--- src/components/UserNotification.vue
+++ src/components/UserNotification.vue
@@ -26,12 +26,7 @@
       </a-popover>
     </div>
     <div class="notify alert">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" @click="() => this.$notification.success({
-        title: '系统通知',
-        content: '暂时不知道通知什么,建议V我50￥,我再告诉你吧！',
-        closable: true,
-        style: { width: '400px' }
-      })">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" @click="handleEmailNotification">
         <path
           d="M467.812 431.851l-36.629-61.056a181.363 181.363 0 01-25.856-93.312V224c0-67.52-45.056-124.629-106.667-143.04V42.667C298.66 19.136 279.524 0 255.993 0s-42.667 19.136-42.667 42.667V80.96C151.716 99.371 106.66 156.48 106.66 224v53.483c0 32.853-8.939 65.109-25.835 93.291L44.196 431.83a10.653 10.653 0 00-.128 10.752c1.899 3.349 5.419 5.419 9.259 5.419H458.66c3.84 0 7.381-2.069 9.28-5.397 1.899-3.329 1.835-7.468-.128-10.753zM188.815 469.333C200.847 494.464 226.319 512 255.993 512s55.147-17.536 67.179-42.667H188.815z" />
       </svg>
@@ -56,6 +51,7 @@
 import { ref, reactive } from "vue";
 import { useUserStore } from '../stores/UserStore';
 const { userInfo } = useUserStore();
+import { Notification } from '@arco-design/web-vue';
 
 const visible = ref(false);
 
@@ -84,8 +80,13 @@ const handleCancel = () => {
 
 // 邮箱通知
 const handleEmailNotification = () => {
-
-};
+      Notification.success({
+        title: '系统通知',
+        content: '暂时不知道通知什么,建议V我50￥,我再告诉你吧！',
+        closable: true,
+        style: { width: '400px' }
+      })
+    }
 </script>
 
 <style scoped>
